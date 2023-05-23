a512f0b88fdf6c3f457f5a119f9b0ef74ad9947e
geetheswar-v <142201025@smail.iitpkd.ac.in>
2023-05-24 00:47:37 +0530

    Added Weather Class to get Weather info using api calls

---
+++ chatmate/utils/weather.py
@@ -0,0 +1,53 @@
+import requests
+import json
+from chatmate.constants.keys import WEATHER_KEY
+
+
+class Weather:
+
+    def __init__(self):
+        self._city = 'london'  # base city
+        self._url = 'https://api.openweathermap.org/data/2.5/weather?'  # open weather base url
+        self._params = {
+            'q': self._city,
+            'appid': WEATHER_KEY,
+            'units': 'metrics'
+        }
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, city_name):
+        self._city = city_name
+
+    def get_response(self):
+        return requests.get(self._url, self._params)
+
+    def get_data(self):
+        response = self.get_response()
+        return json.loads(response.text)
+
+    def is_online(self):
+        return self.get_response().status_code == 200
+
+    def get_coord(self):
+        if self.is_online():
+            return self.get_data()['coord']['lon'], self.get_data()['coord']['lat']
+
+    def get_temp(self):
+        if self.is_online():
+            return self.get_data()['main']['temp']
+
+    def get_dsc(self):
+        if self.is_online():
+            return self.get_data()['weather']['description']
+
+    def get_feel_temp(self):
+        if self.is_online():
+            return self.get_data()['main']['feels_like']
+
+    def get_humidity(self):
+        if self.get_data():
+            return self.get_data()['main']['humidity']
